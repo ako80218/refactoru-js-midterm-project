867339f6ae6d0a84b38cc7521776acf956199e63
ako80218 <andrew@andrewotto.com>
2014-02-11 17:51:41 -0700

    with tag weights

--- model.js
+++ model.js
@@ -1,121 +1,78 @@
 $(function(){
-
-var library = [
-	{	
-	imagePath: 'images/001.jpg',
-	title: 'Opening Ceremony Fireworks',
-	description: 'Opening Ceremony Fireworks over Fisht stadium in Sochi Russia.',
-	photographer: 'Jarrett Frazier',
-	tags: ['Olympics', 'Sochi', 'Sochi Olympics', 'Sochi 2014', 'Winter Olympics', 'Adler', 
-	'Olympic Park', 'Russia', 'Fireworks', 'Opening Ceremony', 'Fisht Stadium', 'Fisht', 'Opening Ceremony']
-
-
-	},
-
-	{
-	imagePath: 'images/002.jpg',
-	title: 'Opening Ceremony Fireworks',
-	description: 'Opening Ceremony Fireworks over Fisht stadium in Sochi Russia.',
-	photographer: 'Jarrett Frazier',
-	tags: ['Olympics', 'Sochi', 'Sochi Olympics', 'Sochi 2014', 'Winter Olympics', 'Adler', 
-	'Olympic Park', 'Russia', 'Fireworks', 'Opening Ceremony', 'Fisht Stadium', 'Fisht', 'Opening Ceremony']
-	},
-
-	{
-	imagePath: 'images/003.jpg',
-	title: 'Sochi 2014 Olympic Coins',
-	description: 'Sochi 2014 Coins Program: 100 rubles showing “Russian winter."',
-	photographer: 'Sochi 2014 Winter Games',
-	tags: ['Olympics', 'Sochi', 'Sochi Olympics', 'Sochi 2014', 'Winter Olympics', 'Russia', 'coins']
-	},
+//creates an image object for each instance of a picture
+var Image = function(obj){
+	for (var key in obj){
+		this[key] = obj[key];
+	}
+}
+//This function loops through an array of image objects and assmbles a tagCounts object.
+//This function returns the tagCounts object.
+var TagCloud = function(arr){
+	this.tagCounts = {};
+	for (var i=0; i<arr.length; i++){
+		for (var j=0; j<arr[i].tags.length; j++){
+			if(this.tagCounts[arr[i].tags[j]] === undefined){
+				this.tagCounts[arr[i].tags[j]]=1;	
+			}else{
+				this.tagCounts[arr[i].tags[j]]++;
+			}
+		}
+	}
 	
-	{
-	imagePath: 'images/004.jpg',
-	title: 'Olympic Torch Relay',
-	description: 'Sochi 2014 Ambassador Tatiana Navka.',
-	photographer: 'Sochi 2014 Winter Games',
-	tags: ['Olympics', 'Sochi', 'Sochi Olympics', 'Sochi 2014', 'Winter Olympics', 'Russia', 'Tatiana Navka', 'Olympic Torch']	
-	},
-	{
-	imagePath: 'images/005.jpg',
-	title: 'Olympic Ice Test',
-	description: 'Sochi 2014 Ambassador & Olympic Champion Tatiana Navka tested first ice on "Iceberg" Arena.',
-	photographer: 'Sochi 2014 Winter Games',
-	tags: ['Olympics', 'Sochi', 'Sochi Olympics', 'Sochi 2014', 'Winter Olympics', 'Russia', 'Tatiana Navka', 'Iceberg Skating Palace']	
-	},
-	{
-	imagePath: 'images/006.jpg',
-	title: 'Children Test The Ice',
-	description: 'Children test the first ice on "Iceberg" Arena.',
-	photographer: 'Sochi 2014 Winter Games',
-	tags: ['Olympics', 'Sochi', 'Sochi Olympics', 'Sochi 2014', 'Winter Olympics', 'Russia', 'Iceberg Skating Palace', 'skaters']
-	},
-
-	{
-	imagePath: 'images/007.jpg',
-	title: 'Sochi Opening Ceremony Skaters',
-	description: 'Skaters entertain during the 2014 Olympic Winter Games Opening Ceremony at Fisht Stadium in Sochi, Russia.',
-	photographer: 'Tim Hipps',
-	tags: ['Olympics', 'Sochi', 'Sochi Olympics', 'Sochi 2014', 'Winter Olympics', 'Russia', 'skaters', 'Opening Ceremony', 'Fisht Stadium', 'Fisht']	
-	},
-
-	{
-	imagePath: 'images/008.jpg',
-	title: 'Doves of Peace upon "Swan Lake."',
-	description: 'Dancers perform to Peter Illyich Tchaikovsky\'s "Swan Lake, during the Opening Ceremony at Fisht Olympic Stadium"',
-	photographer: 'Tim Hipps',
-	tags: ['Olympics', 'Sochi', 'Sochi Olympics', 'Sochi 2014', 'Winter Olympics', 'Russia', 'skaters', 'Opening Ceremony', 'Fisht Stadium', 'Fisht',
-		'Swan Lake', 'Peter Illyich Tchaikovsky']
-	},
-
-	{
-	imagePath: 'images/008.jpg',
-	title: 'Doves of Peace upon "Swan Lake."',
-	description: 'Dancers perform to Peter Illyich Tchaikovsky\'s "Swan Lake," during the Opening Ceremony at Fisht Olympic Stadium',
-	photographer: 'Tim Hipps',
-	tags: ['Olympics', 'Sochi', 'Sochi Olympics', 'Sochi 2014', 'Winter Olympics', 'Russia', 'skaters', 'Opening Ceremony', 'Fisht Stadium', 'Fisht',
-		'Swan Lake', 'Peter Illyich Tchaikovsky', 'Dove of Peace']	
-	},
-	{
-	imagePath: 'images/009.jpg',
-	title: 'Olympic Champion Addresses Young Skaters',
-	description: 'Sochi 2014 Ambassador & Olympic Champion Tatiana Navka addressed young skaters at "Iceberg" Arena.',
-	photographer: 'Sochi 2014 Winter Games',
-	tags: ['Olympics', 'Sochi', 'Sochi Olympics', 'Sochi 2014', 'Winter Olympics', 'Russia', 'skaters', 'Tatiana Navka', 'Iceberg Skating Palace']	
-	},
-	{
-	imagePath: 'images/010.jpg',
-	title: 'Bode\'s Disappointment',
-	description: 'US mens\' alpine skier Bode Miller did not have his best Downhill race at Rosa Khutor Alpine Center on February 9, 2014 in Sochi, Russia.',
-	photographer: 'Al Bello',
-	tags: ['Olympics', 'Sochi', 'Sochi Olympics', 'Sochi 2014', 'Winter Olympics', 'Russia', 'Rosa Khutor', 'Bode Miller']	
-	},
-	{
-	imagePath: 'images/011.jpg',
-	title: 'Julia Lipnitskaia',
-	description: 'Russia\'s Julia Lipnitskaia performs at the Iceberg Skating Palace. ',
-	photographer: 'Al Bello',
-	tags: ['Olympics', 'Sochi', 'Sochi Olympics', 'Sochi 2014', 'Winter Olympics', 'Russia', 'Iceberg Skating Palace', 'Julia Lipnitskaia', 'figure skating']	
-	},
-	{
-	imagePath: 'images/012.jpg',
-	title: 'Sochi Olympics Figure Skating',
-	description: 'Meryl Davis, left, and Charlie White, of the United States, perform their free dance.',
-	photographer: 'Paul Chiasson',
-	tags: ['Olympics', 'Sochi', 'Sochi Olympics', 'Sochi 2014', 'Winter Olympics', 'Russia', 'Iceberg Skating Palace', 'Meryl Davis', 'Charlie White', 'figure skating']	
-	}
-
-];
-
-var collageItem= filter(library, function(){
-	for (var i=0; i<library.length; i++){
-		return 
-	}
-
-});
+};
+//This function calculates the most frequent tag in the TagCloud object
+//then it calculates the relative weight of all other tags in the cloud and 
+//places those values in a fontSizes object. It returns the tagWeights object. 
+TagCloud.prototype.setTagWeights = function(){
+	var maxProp = null;
+	var maxValue = -1;
+	for(var key in this.tagCounts){	
+		var value = this.tagCounts[key];
+		if (value > maxValue) {
+		      maxValue = value;
+		      maxProp = key;
+    		}
+    	}
+    	var tagWeights ={};
+    	for(var key in this.tagCounts){
+    		tagWeights.key =key;
+    		tagWeights[key]=this.tagCounts[key]/maxValue;
+    	}
+    	console.log("tagWeights: ", tagWeights);
+    	return tagWeights;
+}
+//This function assigns font sizes based on weights assigned to 
+//tags in the setTagWeights method. This function takes as its argument, the 
+//object that setTagWeights returns. The setFontSizes method returns an  
+TagCloud.prototype.setFontSizes = function(obj){
+	for(var key in obj){
+		if(obj[key] ===1){}	
+}
+//empty image catalogue
+var image = [];
+for (i=0; i<library.length; i++){
+	//create a catalogue of all the image objects
+	var imageObject= new Image(library[i]);
+	image.push(imageObject);
+}
 $(document).on('click', '#photo-search-submit', function(e){
 	e.preventDefault();
 	var searchTerm = $('#photo-search').val();
+	var pastiche = [];
+	//the pastiche is an array of image objects with tags values that 
+	//are exactly equal to the search term
+	pastiche= filter(image, function(item){
+			return item.tags.indexOf(searchTerm) !== -1;
+		});
+	// console.log(pastiche);
+	var pasticheTagCloud = new TagCloud(pastiche);
+	console.log("pasticheTagCloud: ", pasticheTagCloud);
+	$('<ul id="tag-list" >').appendTo("#tagCloud");
+	//create item
+	var li = $("<li>");
+	//add to list
+	li.appendTo("#tag-list");
+	pasticheTagCloud.setTagWeights();
 });
 
 
